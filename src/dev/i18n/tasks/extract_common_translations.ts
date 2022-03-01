3e70086e2aa09c2cbeb8cf7a6f232f75125ce682
Or Ouziel <or.ouziel@elastic.co>
2022-03-01 02:15:51 +0200

    wip

--- src/dev/i18n/tasks/extract_common_translations.ts
+++ src/dev/i18n/tasks/extract_common_translations.ts
@@ -14,6 +14,19 @@ import { writeFileAsync } from '..';
 
 const ex = promisify(exec);
 
+// TODO:
+// - try 1 node script with formatjs/extract
+// - fix eslint/ts errors
+// - stats/metrics for bundle size, memory usage, build time, check for diffs
+
+// - transform:
+//  - fix eslint-dot-notation (i18n.common["Foo"])
+//  - fix missing transform: import { i18n as t } from '@kbn/i18n';
+//  - add import { i18n } from '@kbn/i18n and remove { FormattedMessage } from 'i18n/react-common'
+
+// TODO: alternative ?
+const ES_COMMON_LOCALE_PATH = resolve('packages/kbn-i18n/src/core/en-common.json');
+
 function getCommonMessages(
   idMap: Map<string, [file: string, msg: { message: string }]>
 ): Array<[string, string[]]> {
@@ -41,9 +54,8 @@ export function extractCommonDefaultMessages(): ListrTask[] {
       task: (ctx) => {
         const allMessages = getCommonMessages(ctx.messages);
         const commonMessages = allMessages.filter(isCommonTranslation);
-        ctx.commonMessages = commonMessages; // TODO: ???
-        // TODO: write to kbn-i18n/en-common.json
-        // return writeFileAsync('foo.json', JSON.stringify(commonMessages));
+        ctx.commonMessages = commonMessages; // TODO: types
+        return writeFileAsync(ES_COMMON_LOCALE_PATH, JSON.stringify(commonMessages));
         // TODO: need to build kbn18 afterwards
       },
     },
@@ -52,26 +64,27 @@ export function extractCommonDefaultMessages(): ListrTask[] {
       title: 'Transform to i18n.common',
       task: (ctx) => new Listr(ctx.commonMessages.map(createTransformTask)),
     },
-    // {
-    //   enabled: (ctx) => !!ctx.commonMessages && !!ctx.transformCommon,
-    //   title: 'run i18n_check --fix',
-    //   task: () => ex('node scripts/i18n_check --fix'),
-    // },
-    // {
-    //   enabled: (ctx) => !!ctx.commonMessages && !!ctx.transformCommon,
-    //   title: 'Prettify',
-    //   task: (ctx) => {
-    //     const fileList = [...new Set(ctx.commonMessages.flatMap(([, files]) => files))].join(' ');
-    //     return ex(`node_modules/.bin/prettier --write ${fileList}`);
-    //   },
-    // },
+    {
+      enabled: (ctx) => !!ctx.commonMessages && !!ctx.transformCommon,
+      title: 'run i18n_check --fix',
+      // TODO: call script directly
+      task: () => ex('node scripts/i18n_check --fix'),
+    },
+    {
+      enabled: (ctx) => !!ctx.commonMessages && !!ctx.transformCommon,
+      title: 'Prettify',
+      task: (ctx) => {
+        const fileList = [...new Set(ctx.commonMessages.flatMap(([, files]) => files))].join(' ');
+        // TODO: use prettier module
+        return ex(`node_modules/.bin/prettier --write ${fileList}`);
+      },
+    },
   ];
 }
 
-const createTransformTask = ([msg, files]: [string, string[]], i: number) => ({
-  // title: `\n message: \t ${msg} \n files: \t ${files.length} `,
-  title: msg,
-  task: () => transform(msg, files, i),
+const createTransformTask = ([title, files]: [string, string[]], i: number) => ({
+  title,
+  task: () => transform(title, files, i),
 });
 
 // TODO: exclude {plural} and others
@@ -80,6 +93,6 @@ const isCommonTranslation = ([msg, files]: [string, string[]]) => files.length >
 // TODO: needed?
 const quote = (v: string) => (v.includes("'") ? `"${v}"` : `'${v}'`);
 
+// TODO: use babel, remove i18n_transform1
 const transform = (msg: string, files: string[], idx: number) =>
-  // TODO: replace jscodeshift ?
   ex(`jscodeshift -t ${resolve('i18n_transform1.js')} ${files.join(' ')} --word=${quote(msg)}`);
