6e65047a234451882500ba682589f5ed51fd7c92
uri-weisman <uri.weisman@elastic.co>
2022-03-02 15:25:00 +0200

    fix paths post main merge

--- x-pack/plugins/cloud_security_posture/server/saved_objects/csp_rule_template.ts
+++ x-pack/plugins/cloud_security_posture/server/saved_objects/csp_rule_template.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
@@ -39,3 +40,47 @@ export const cspRuleTemplateAssetType: SavedObjectsType<CspRuleTemplateSchema> =
   namespaceType: 'agnostic',
   mappings: ruleTemplateAssetSavedObjectMappings,
 };
+||||||| parent of 03324579a89 (fix paths post main merge)
+=======
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { SavedObjectsType } from '../../../../../../src/core/server';
+import {
+  type CspRuleTemplateSchema,
+  cspRuleTemplateSavedObjectType,
+} from '../../../common/schemas/csp_rule_template';
+
+const ruleTemplateAssetSavedObjectMappings: SavedObjectsType<CspRuleTemplateSchema>['mappings'] = {
+  dynamic: false,
+  properties: {
+    name: {
+      type: 'text', // search
+      fields: {
+        // TODO: how is fields mapping shared with UI ?
+        raw: {
+          type: 'keyword', // sort
+        },
+      },
+    },
+    description: {
+      type: 'text',
+    },
+  },
+};
+
+export const cspRuleTemplateAssetType: SavedObjectsType<CspRuleTemplateSchema> = {
+  name: cspRuleTemplateSavedObjectType,
+  hidden: false,
+  management: {
+    importableAndExportable: true,
+    visibleInManagement: false,
+  },
+  namespaceType: 'agnostic',
+  mappings: ruleTemplateAssetSavedObjectMappings,
+};
+>>>>>>> 03324579a89 (fix paths post main merge)
