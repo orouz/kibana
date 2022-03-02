acadcacd3ccfcac3d22b67802b2816a8ec9634a6
uri-weisman <uri.weisman@elastic.co>
2022-03-02 17:32:43 +0200

    CR changes

---
+++ x-pack/plugins/cloud_security_posture/server/saved_objects/cis_1_4_1/csp_rule_template.ts
@@ -0,0 +1,62 @@
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { SavedObjectsType } from '../../../../../../src/core/server';
+import {
+  type CspRuleTemplateSchema,
+  cspRuleTemplateSavedObjectType,
+} from '../../../common/schemas/csp_rule_template';
+
+const ruleTemplateAssetSavedObjectMappings: SavedObjectsType<CspRuleTemplateSchema>['mappings'] = {
+  dynamic: false,
+  properties: {
+    name: {
+      type: 'text',
+    },
+    description: {
+      type: 'text',
+    },
+    rationale: {
+      type: 'text',
+    },
+    impact: {
+      type: 'text',
+    },
+    default_value: {
+      type: 'text',
+    },
+    remediation: {
+      type: 'text',
+    },
+    benchmark: {
+      type: 'object',
+    },
+    severity: {
+      type: 'text',
+    },
+    benchmark_rule_id: {
+      type: 'text',
+    },
+    rego_rule_id: {
+      type: 'text',
+    },
+    tags: {
+      type: 'text',
+    },
+  },
+};
+
+export const cspRuleTemplateAssetType: SavedObjectsType<CspRuleTemplateSchema> = {
+  name: cspRuleTemplateSavedObjectType,
+  hidden: false,
+  management: {
+    importableAndExportable: true,
+    visibleInManagement: false,
+  },
+  namespaceType: 'agnostic',
+  mappings: ruleTemplateAssetSavedObjectMappings,
+};
