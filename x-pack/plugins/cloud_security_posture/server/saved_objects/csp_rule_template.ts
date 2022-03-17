c2c041b747f0cdf7429a54b777c671283cdb4724
Ido Cohen <ido.cohen@elastic.co>
2022-03-17 12:10:47 +0200

    update template path

--- x-pack/plugins/cloud_security_posture/server/saved_objects/csp_rule_template.ts
+++ x-pack/plugins/cloud_security_posture/server/saved_objects/csp_rule_template.ts
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /*
  * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
  * or more contributor license agreements. Licensed under the Elastic License
@@ -84,3 +85,35 @@ export const cspRuleTemplateAssetType: SavedObjectsType<CspRuleTemplateSchema> =
   mappings: ruleTemplateAssetSavedObjectMappings,
 };
 >>>>>>> 03324579a89 (fix paths post main merge)
+||||||| parent of e7da7d89366 (update template path)
+=======
+/*
+ * Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+ * or more contributor license agreements. Licensed under the Elastic License
+ * 2.0; you may not use this file except in compliance with the Elastic License
+ * 2.0.
+ */
+
+import { SavedObjectsType } from '../../../../../src/core/server';
+import {
+  type CloudSecurityPostureRuleTemplateSchema,
+  cloudSecurityPostureRuleTemplateSavedObjectType,
+} from '../../common/schemas/csp_rule_template';
+
+const ruleTemplateAssetSavedObjectMappings: SavedObjectsType<CloudSecurityPostureRuleTemplateSchema>['mappings'] =
+  {
+    dynamic: false,
+    properties: {},
+  };
+
+export const cspRuleTemplateAssetType: SavedObjectsType<CloudSecurityPostureRuleTemplateSchema> = {
+  name: cloudSecurityPostureRuleTemplateSavedObjectType,
+  hidden: false,
+  management: {
+    importableAndExportable: true,
+    visibleInManagement: false,
+  },
+  namespaceType: 'agnostic',
+  mappings: ruleTemplateAssetSavedObjectMappings,
+};
+>>>>>>> e7da7d89366 (update template path)
